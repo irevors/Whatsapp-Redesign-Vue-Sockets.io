63f3a1b826ece98609f86591c73b93e3b17bb3cd
Ignacio Riveros <ignacio.riveros.farfan@gmail.com>
2021-09-30 16:03:12 -0300

    change component stylings to fit mockup

--- frontend/src/components/Groups/GroupCard.vue
+++ frontend/src/components/Groups/GroupCard.vue
@@ -1,10 +1,15 @@
 <template>
   <article class="container C-group-card">
-    <h5 class="C-group-card__title">{{ title }}</h5>
-    <Avatar
-      :src="require('../../assets/logo.png')"
-      class="C-group-card__group-icon"
-    />
+    <button class="C-group-card__actions btn rounded-circle">
+      <font-awesome-icon
+        :icon="['fas', 'ellipsis-v']"
+        class="C-group-card__actions-icon"
+      />
+    </button>
+    <header class="C-group-card__header">
+      <Avatar :src="groupImg" class="C-group-card__group-icon" />
+      <h5 class="C-group-card__title">{{ title }}</h5>
+    </header>
     <ul class="C-group-card__members-list">
       <li
         v-for="(member, index) in iterableMembers"
@@ -17,7 +22,7 @@
           :padding="1"
         />
       </li>
-      <li class="C-group-card__members-li">
+      <li class="C-group-card__members-li" v-if="unshownMembers">
         <Avatar
           class="C-group-card__members-avatar"
           :text="`+${unshownMembers}`"
@@ -26,9 +31,18 @@
         />
       </li>
     </ul>
-    <hr />
-    <h6 class="C-group-card__last-sender">{{ lastMessage.sender }}</h6>
-    <p class="C-group-card__last-msg">{{ lastMessage.msg }}</p>
+    <hr class="C-group-card__separator" />
+    <footer class="C-group-card__footer">
+      <h6 class="C-group-card__last-sender">{{ lastMessage.sender }}</h6>
+      <p class="C-group-card__last-msg">{{ lastMessage.msg }}</p>
+
+      <Avatar
+        class="C-group-card__unread-msgs"
+        :text="`${unreadMessages}`"
+        :padding="0.8"
+        :fontSize="0.6"
+      />
+    </footer>
   </article>
 </template>
 
@@ -36,6 +50,11 @@
 export default {
   props: {
     title: { type: String, default: "Titulo de la tarjeta" },
+    groupImg: {
+      type: String,
+      default:
+        "https://assets.codepen.io/t-1003/internal/avatars/teams/default.png?fit=crop&format=auto&height=256&version=1513627136&width=256",
+    },
     members: {
       type: Array,
       default() {
@@ -81,13 +100,41 @@ export default {
 <style lang="scss">
 .C-group-card {
   border-radius: 1.5rem;
+  position: relative;
   background-color: var(--color__bg);
+  padding: 1rem;
+  width: 90%;
+  border: 1px solid #f4f4ff;
+  border: 1px solid var(--color__group-card-border);
+  margin-bottom: 1rem;
+
+  &__actions {
+    position: absolute;
+    right: 30px;
+    top: 30px;
+    padding: 1rem;
+  }
+
+  &__actions-icon {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 0.9rem;
+    color: var(--color__navbar-btn-icons);
+  }
+  &__header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
   &__group-icon {
     display: inline-block;
   }
   &__members-list {
     display: flex;
     list-style-type: none;
+    padding-left: 0;
   }
   &__members-li {
     &:nth-child(2) {
@@ -102,6 +149,33 @@ export default {
   }
   &__members-avatar {
     border: 2.5px solid var(--color__bg);
+  }
+  &__title {
+    color: var(--color__main-view-primary-text);
+    margin: 0 2rem 0 1rem;
+    max-width: 100%;
+  }
+  &__separator {
+    color: #e5e5eb;
+    opacity: 0.5;
+    height: 1.2px !important;
+    color: var(--color__group-card-separator);
+  }
+  &__footer {
+    position: relative;
+  }
+  &__last-sender {
+    color: var(--color__main-view-primary-text);
+  }
+  &__last-msg {
+    color: var(--color__main-view-secondary-text);
+  }
+  &__unread-msgs {
+    position: absolute;
+    color: #fff;
+    right: 30px;
+    top: 50%;
+    transform: translateY(-50%);
   }
 }
 </style>
